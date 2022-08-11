0ec460ed16f37d0d9612a6073ec74d80b2797e17
MingXuanSu <mxsu@jihulab.com>
2022-08-11 12:01:07 +0800

    记录 remote_ip 和 request_uuid

--- lib/logisticed/logistic.rb
+++ lib/logisticed/logistic.rb
@@ -2,7 +2,7 @@ module Logisticed
   class Logistic < ::ActiveRecord::Base
     belongs_to :source, polymorphic: true
     belongs_to :operator, polymorphic: true
-    before_create :set_logistic_user
+    before_create :set_logistic_user, :set_request_info
 
     def self.as_user(user)
       last_logisticed_user = ::Logisticed.store[:logisticed_user]
@@ -18,6 +18,11 @@ module Logisticed
       self.operator ||= ::Logisticed.store[:logisticed_user] # from .as_user
       self.operator ||= ::Logisticed.store[:current_user].try!(:call) # from Sweeper
       nil # prevent stopping callback chains
+    end
+
+    def set_request_info
+      self.remote_ip    = ::Logisticed.store[:current_remote_address]
+      self.request_uuid = ::Logisticed.store[:current_request_uuid]
     end
   end
 end
